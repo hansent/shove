16ef0533fbf8ee6732b1f8a7b64159a89f108ba1
kwarterthieves <kwarterthieves@yahoo.com>
2006-11-26 20:54:59 -0700

    [svn]
    --HG--
    branch : shove

--- trunk/shove/cache/bsddb.py
+++ trunk/shove/cache/bsddb.py
@@ -1,29 +1,48 @@
 import bsddb
 try:
+    from cStringIO import StringIO
+except ImportError:
+    from cStringIO import StringIO
+try:
     import cPickle as pickle
 except ImportError:
     import pickle
 from shove import synchronized
-from shove.store.memory import MemoryStore
+from shove.cache.memory import MemoryCache
+
+__all__ = ['BsddbCache']
 
 
-class BsddbStore(MemoryStore):
+class BsddbCache(MemoryCache):
 
     def __init__(engine, **kw):
-        super(BsddbStore, self).__init__(engine, **kw)
+        super(BsddbCache, self).__init__(engine, **kw)
         db = engine.split('/', 2)
-        self._store = bsddb.htopen(db)
-
+        self._cache = bsddb.htopen(db)
+        
     @synchronized
     def __getitem__(self, key):
-        return pickle.loads(self._store[key])
+        '''Fetch a given key from the cache.  If the key does not exist, return
+        default, which itself defaults to None.
 
-    @synchronized        
-    def __setitem__(self, key, value):        
-        self._store[key] = pickle.dumps(value)
-        self._store.sync()
-
+        @param key Keyword of item in cache.
+        @param default Default value (default: None)
+        '''
+        local = StringIO(self._cache[key])
+        exp, now = pickle.load(local), time.time()
+        # Remove item if time has expired.
+        if exp < now: del self._cache[key]        
+        return pickle.load(f)
+                
     @synchronized
-    def __delitem__(self, key):
-        del self._store[key]
-        self._store.sync()
+    def __setitem__(self, key, value):
+        '''Set a value in the cache.
+
+        @param key Keyword of item in cache.
+        @param value Value to be inserted in cache.        
+        '''
+        if len(self._store) > self._max_entries: self._cull()
+        local = StringIO()
+        pickle.dump(time.time() + self.timeout, local, 2)
+        pickle.dump(value, local, 2)
+        self._cache[key] = local.getvalue()
