9d0e83264e33bc9fc01e1e75fd03ba4d0a10807c
kwarterthieves <kwarterthieves@yahoo.com>
2006-11-26 19:17:39 -0700

    [svn]
    --HG--
    branch : shove

--- trunk/shove/store/bsddb.py
+++ trunk/shove/store/bsddb.py
@@ -3,16 +3,27 @@ try:
     import cPickle as pickle
 except ImportError:
     import pickle
-from shove.store.simple import SimpleStore
+from shove import synchronized
+from shove.store.memory import MemoryStore
 
 
-class BsddbStore(SimpleStore):
+class BsddbStore(MemoryStore):
 
     def __init__(engine, **kw):
         super(BsddbStore, self).__init__(engine, **kw)
+        db = engine.split('/', 2)
+        self._store = bsddb.htopen(db)
 
-   def __getitem__(self, key):
-       return pickle.loads(self._store[key])
-        
+    @synchronized
+    def __getitem__(self, key):
+        return pickle.loads(self._store[key])
+
+    @synchronized        
     def __setitem__(self, key, value):        
         self._store[key] = pickle.dumps(value)
+        self._store.sync()
+
+    @synchronized
+    def __delitem__(self, key):
+        del self._store[key]
+        self._store.sync()
