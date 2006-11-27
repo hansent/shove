00f8afbfb378e992642f5ab0ae9ce53f91112763
tomalok <none@none>
2006-11-26 20:31:21 -0700

    [svn]
    --HG--
    branch : shove

--- trunk/shove/store/zodb.py
+++ trunk/shove/store/zodb.py
@@ -1,22 +1,22 @@
 from ZODB import FileStorage, DB
 import transaction
-from shove import BaseStore
+from shove.simple import SimpleStore
+
+__all__ = ['ZodbStore']
 
 
 class ZodbStore(BaseStore):
 
     def __init__(self, engine, **kw):
+        super(ZodbStore, self).__init__(engine, **kw)
         db = engine.split('/', 2)
         self._storage = FileStorage.FileStorage(db)
-        self._db = DB(storage)
-        self._connection = db.open()
-        self._store = connection.root()
-
-    def __getitem__(self, key):
-        return pickle.loads(self._store[key])
+        self._db = DB(self._storage)
+        self._connection = self._db.open()
+        self._store = self._connection.root()
 
     def __setitem__(self, key, value):        
-        self._store[key] = pickle.dumps(value)
+        self._store[key] = value
         transaction.commit()
 
     def __delitem__(self, key):
