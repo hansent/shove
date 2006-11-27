fdaa2a7aef1f2c8a83e476c77bd211a08d1414a8
kwarterthieves <kwarterthieves@yahoo.com>
2006-11-27 16:15:21 -0700

    [svn]
    --HG--
    branch : shove
    rename : trunk/shove/store/bsddb.py => trunk/shove/store/bsdb.py

--- trunk/shove/store/bsdb.py
+++ trunk/shove/store/bsdb.py
@@ -7,22 +7,22 @@ from shove import synchronized
 from shove.store.memory import MemoryStore
 
 
-class BsddbStore(MemoryStore):
+class BsdbStore(MemoryStore):
 
-    def __init__(engine, **kw):
-        super(BsddbStore, self).__init__(engine, **kw)
+    def __init__(self, engine, **kw):
+        super(BsdbStore, self).__init__(engine, **kw)
         self._store = bsddb.htopen(engine.split('/', 2))
 
     @synchronized
     def __getitem__(self, key):
-        return pickle.loads(super(BsddbStore, self).__getitem__(key))
+        return pickle.loads(super(BsdbStore, self).__getitem__(key))
 
     @synchronized        
     def __setitem__(self, key, value):        
-        super(BsddbStore, self).__setitem__(key, pickle.dumps(value))
+        super(BsdbStore, self).__setitem__(key, pickle.dumps(value))
         self._store.sync()
 
     @synchronized
     def __delitem__(self, key):
-        super(BsddbStore, self).__delitem__(key)
+        super(BsdbStore, self).__delitem__(key)
         self._store.sync()
